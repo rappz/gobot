fd6a61de2751c0ce7d06a4692113bbeb483e688c
Zachary Rapp <zachary@rapplab.xyz>
2026-02-13 22:41:13 -0800

    fixed env

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require golang.org/x/text v0.12.0 // indirect
 
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/joho/godotenv v1.5.1
 	github.com/subosito/gotenv v1.6.0
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.5.0 // indirect
--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 
-	"github.com/subosito/gotenv"
+	"github.com/joho/godotenv"
 	bot "rapplab.xyz/chrisbot/bot"
 )
 
-func init() {
-	gotenv.Load()
-}
-
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Print("Note: .env file not loaded (run from project root or set env vars): ", err)
+	}
+	fmt.Println("Starting bot...")
+	fmt.Println("Token: ", os.Getenv("TOKEN"))
 	bot.BotToken = os.Getenv("TOKEN")
 	bot.Run() // call the run function of bot/bot.go
+	fmt.Println("Bot started")
 }
