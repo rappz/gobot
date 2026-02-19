45fc7fd02d721e078540582238db9d4a04c41582
Zachary Rapp <zachary@rapplab.xyz>
2026-02-19 00:03:28 -0800

    added voice

--- bot/voice.go
+++ bot/voice.go
@@ -1,17 +1,24 @@
 package bot
 
 import (
+	"encoding/binary"
+	"fmt"
+	"io"
 	"log"
+	"math/rand"
 	"os"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-func getVoiceClips() ([]string, error) {
+var buffer = make([][]byte, 0)
+
+func getVoiceClip() error {
 	files, err := os.ReadDir("media")
 	if err != nil {
 		log.Printf("Error reading media directory: %v", err)
-		return nil, err
+		return err
 	}
 	var names []string
 	for _, file := range files {
@@ -19,7 +26,46 @@ func getVoiceClips() ([]string, error) {
 			names = append(names, file.Name())
 		}
 	}
-	return names, nil
+	file, err := os.Open(files[rand.Intn(len(files))].Name())
+	if err != nil {
+		log.Printf("Error reading media file: %v", err)
+		return err
+	}
+	defer file.Close()
+
+	var opuslen int16
+
+	for {
+		// Read opus frame length from dca file.
+		err = binary.Read(file, binary.LittleEndian, &opuslen)
+
+		// If this is the end of the file, just return.
+		if err == io.EOF || err == io.ErrUnexpectedEOF {
+			err := file.Close()
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+
+		if err != nil {
+			fmt.Println("Error reading from dca file :", err)
+			return err
+		}
+
+		// Read encoded pcm from dca file.
+		InBuf := make([]byte, opuslen)
+		err = binary.Read(file, binary.LittleEndian, &InBuf)
+
+		// Should not be any end of file errors
+		if err != nil {
+			fmt.Println("Error reading from dca file :", err)
+			return err
+		}
+
+		// Append encoded pcm data to the buffer.
+		buffer = append(buffer, InBuf)
+	}
 
 }
 
@@ -29,8 +75,16 @@ func voiceKing(s *discordgo.Session, guildID, channelID string) {
 		log.Printf("Error joining voice channel: %v", err)
 		return
 	}
-	defer voice.Disconnect()
+	getVoiceClip()
+
+	time.Sleep(250 * time.Millisecond) // Wait for the bot to join the voice channel
 	voice.Speaking(true)
-	defer voice.Speaking(false)
-	voice.Speaking(true)
+
+	for _, buff := range buffer {
+		voice.OpusSend <- buff
+	}
+
+	voice.Speaking(false)
+	time.Sleep(250 * time.Millisecond)
+	voice.Disconnect()
 }
